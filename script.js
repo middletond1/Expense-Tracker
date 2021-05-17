72994a8428f10e1516de4425e8adc3c45b36ae1b
David Middleton <middleton.davida@gmail.com>
2021-05-16 20:13:49 -0400

    added build table and draw table functions, and delete button creation and functionality

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function getExpense() {
 }
 
 function getAmount() {
-    const amount = parseFloat(document.querySelector('#amount').value).toFixed(2);
+    const amount = `$${parseFloat(document.querySelector('#amount').value).toFixed(2)}`;
     return amount;
 }
 
@@ -53,24 +53,54 @@ function getStore() {
 
 function createTableData(item) {
     const tableData = document.createElement('td');
-    return tableData.textContent(`${item}`);
+    tableData.appendChild(document.createTextNode(item));
+    return tableData;
 }
 
 function createTableRow() {
     const tableRow = document.createElement('tr');
+    tableRow.classList.add('row');
     return tableRow;
 }
 
+function createDeleteButton() {
+    const deleteButton = document.createElement('button');
+    deleteButton.classList = 'delete';
+    deleteButton.appendChild(document.createTextNode('Remove'));
+    return deleteButton;
+}
+
+function deleteRow(element) {
+    if (element.target.classList.contains('delete')) {
+        element.target.parentElement.remove();
+    }
+}
+
 function buildTable(expense, amount, date, store) {
-    const table = document.querySelector('#expensetable').appendChild(createTableRow());
-    table.appendChild(createTableData(expense));
-    table.appendChild(createTableData(amount));
-    table.appendChild(createTableData(date));
-    table.appendChild(createTableData(store));
+    const row = createTableRow();
+    const table = document.querySelector('#expensetable');
+    row.appendChild(createTableData(expense));
+    row.appendChild(createTableData(amount));
+    row.appendChild(createTableData(date));
+    row.appendChild(createTableData(store));
+    row.appendChild(createDeleteButton());
+    table.appendChild(row);
 }
 
 function drawTable() {
-
+    const expense = getExpense();
+    const amount = getAmount();
+    const date = `${monthToCalendar(parseMonth(getDate()))} ${parseDate(getDate())}, ${parseYear(getDate())}`;
+    const store = getStore();
+    if (expense === '' || amount === `$${NaN}` || getDate() == '') {
+        return;
+    }
+    buildTable(expense, amount, date, store);
+    document.querySelector('#expense').value = '';
+    document.querySelector('#amount').value = '';
+    document.querySelector('#date').value = '';
+    document.querySelector('#store').value = '';
 }
 
-document.querySelector('#submitbutton').addEventListener('click', );
+document.querySelector('#submitbutton').addEventListener('click', drawTable);
+document.querySelector('#expensetable').addEventListener('click', deleteRow);
